5fca1afc0519ccfee2ace047e2ffa9ac8afb861a
Radek <r.szkorla@gmail.com>
2016-11-28 12:22:13 +0100

    1

--- Program.cs
+++ Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace labki2811
 {
+  class Bisekcja
+  {
+    public double A { get; set; }
+    public double B { get; set; }
+    public double eps { get; set; }
+    public delegate double DelegatFuncj(double x);
+
+    public DelegatFuncj F { get; set; }
+
+    public Bisekcja(double a, double b, DelegatFuncj f , double e = 1e-9)
+    {
+      A = a;
+      B = b;
+      F = f;
+      eps = e;
+    }
+  }
+
   class Program
   {
+
+    
     static void Main(string[] args)
     {
+      Bisekcja kont = new Bisekcja();
+      double x0 = 0;
+      kont.F = x => x * x;
+      do
+      {
+        
+      } while (Math.Abs(kont.F(x0))<kont.eps|| );
+
     }
   }
 }
