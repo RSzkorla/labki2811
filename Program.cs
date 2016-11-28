66b30e4d374f74ffa8cb627fea51d9cec21dda40
Radek <r.szkorla@gmail.com>
2016-11-28 12:34:56 +0100

    3

--- Program.cs
+++ Program.cs
@@ -8,19 +8,54 @@ namespace labki2811
 {
   class Bisekcja
   {
-    private double A { get; set; }
-    private double B { get; set; }
+    private double _a { get; set; }
+    private double _b { get; set; }
     private double eps { get; set; }
 
     public Func<double,double> F { get; set; }
 
-    public Bisekcja(double a, double b, DelegatFuncj f , double e = 1e-9)
+    public Bisekcja(double a, double b , double e = 1e-9)
     {
-      A = a;
-      B = b;
-      F = f;
+      _a = a;
+      _b = b;
       eps = e;
     }
+    public double Solve()
+    {
+      if (F==null)
+      {
+        throw new ArgumentException();
+      }
+      if (F(_a)*F(_b) >0 )
+      {
+        throw new ArgumentOutOfRangeException();
+      }
+      double x0;
+      do
+      {
+        double mid = (_a + _b) / 2;
+
+        x0 = mid;
+
+        if (F(mid) < eps)
+        {
+          x0 = mid;
+          break;
+        }
+
+        if (F(_a)*F(mid)<0)
+        {
+          _b = mid;
+        }
+        else
+        {
+          _a = mid;
+        }
+      } while (Math.Abs(_a- _b)< eps);
+      return x0;
+    }
+
+
   }
 
   class Program
@@ -29,13 +64,8 @@ namespace labki2811
     
     static void Main(string[] args)
     {
-      Bisekcja kont = new Bisekcja();
-      double x0 = 0;
-      kont.F = x => x * x;
-      do
-      {
-        
-      } while (Math.Abs(kont.F(x0))<kont.eps|| );
+      Bisekcja kont = new Bisekcja((2/3f)*Math.PI, (3/2f)*);
+
 
     }
   }
